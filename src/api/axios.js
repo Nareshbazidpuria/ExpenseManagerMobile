a73122a0d679a5ef00b196f37e215a0fe18e4cd9
Naresh <nareshbazidpuria@gmail.com>
2024-07-06 11:40:09 +0530

    download issue

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,19 +2,19 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
 import { navigateRef } from "../../App";
 
-// export const baseURL = "http://192.168.254.171:4040";
-export const baseURL = "https://expensemanager-047k.onrender.com";
+export const baseURL = "http://192.168.185.171:4040";
+// export const baseURL = "https://expensemanager-047k.onrender.com";
 
 export const Axios = axios.create({ baseURL });
 
 Axios.interceptors.response.use(
   (response) => response,
   async (error) => {
-    if (error?.response?.data?.data?.update)
-      navigateRef?.current.navigate(
-        "DownloadApk",
-        error?.response?.data?.data?.update
-      );
+    // if (error?.response?.data?.data?.update)
+    //   navigateRef?.current.navigate(
+    //     "DownloadApk",
+    //     error?.response?.data?.data?.update
+    //   );
 
     if (error?.response?.status === 401) {
       await AsyncStorage.clear();
